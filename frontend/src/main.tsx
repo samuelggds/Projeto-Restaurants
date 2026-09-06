b75ec85414543f813688a38c4c403b52c3bd01b4
Samuel <123962810+samuelggds@users.noreply.github.com>
2026-09-06 07:51:50 -0300

    feat: replace toast container with app notice viewport

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { GlobalStyles } from '../GlobalStyles/globalStyles.js';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import './config/sentry.js';
 import AppRoutes from './routes/AppRoutes.js';
 import { AuthProvider } from './contexts/authContext.js';
 import { AppDialogProvider } from './components/AppDialog/AppDialogProvider.js';
+import { AppNoticeViewport } from './components/AppNotice/AppNoticeViewport.js';
 import AppRuntimeBoundary from './components/AppRuntimeBoundary/AppRuntimeBoundary.js';
 import {
   installVitePreloadRecovery,
@@ -31,18 +30,7 @@ createRoot(rootElement).render(
     <AppRuntimeBoundary>
       <AuthProvider>
         <AppDialogProvider>
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={false}
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="dark"
-          />
+          <AppNoticeViewport />
           <GlobalStyles />
           <AppRoutes />
         </AppDialogProvider>
