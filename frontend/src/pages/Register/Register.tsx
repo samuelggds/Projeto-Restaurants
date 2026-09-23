a1fd9c9ba23b0a8b3272aa0299a917300ba8ecda
Samuel <123962810+samuelggds@users.noreply.github.com>
2026-09-23 17:12:58 -0300

    feat(ui): add visibility toggle to password fields in Register.tsx

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,4 +1,5 @@
 import { type FormEvent, useLayoutEffect, useMemo, useState } from 'react';
+import { PasswordVisibilityField } from '../../components/PasswordVisibilityField/PasswordVisibilityField';
 import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { ThemeProvider } from 'styled-components';
@@ -286,7 +287,8 @@ export default function Register() {
                   <S.LoginInputIcon aria-hidden="true">
                     <LockKeyhole />
                   </S.LoginInputIcon>
-                  <S.Input
+                  <PasswordVisibilityField label="senha">
+                    <S.Input
                     id="password"
                     type="password"
                     placeholder="Mínimo 8 caracteres"
@@ -299,7 +301,8 @@ export default function Register() {
                     aria-describedby="register-password-requirements"
                     disabled={isSubmitting}
                     required
-                  />
+                    />
+                  </PasswordVisibilityField>
                 </S.LoginInputField>
               </S.InputGroup>
 
@@ -309,7 +312,8 @@ export default function Register() {
                   <S.LoginInputIcon aria-hidden="true">
                     <LockKeyhole />
                   </S.LoginInputIcon>
-                  <S.Input
+                  <PasswordVisibilityField label="senha">
+                    <S.Input
                     id="confirmPassword"
                     type="password"
                     placeholder="Repita sua senha"
@@ -322,7 +326,8 @@ export default function Register() {
                     aria-describedby="register-password-requirements"
                     disabled={isSubmitting}
                     required
-                  />
+                    />
+                  </PasswordVisibilityField>
                 </S.LoginInputField>
               </S.InputGroup>
 
